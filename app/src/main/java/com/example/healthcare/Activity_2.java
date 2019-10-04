58db1a3775d40d6b863d53be2aab4baf06d84f71
StanislavGardev <sigma77dev@gmail.com>
2019-10-04 13:15:06 +0300

    Menu item changes

--- app/src/main/java/com/example/healthcare/Activity_2.java
+++ app/src/main/java/com/example/healthcare/Activity_2.java
@@ -16,10 +16,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity_2 extends AppCompatActivity implements View.OnClickListener{
-    ImageButton btn1,btn2,btn3,btn4,btn5,btn6;
+    ImageButton btn3,btn3_2,btn4,btn5,btn6;
     TextView row0,row1,row2,row3,row3_1,row3_2,row321,row322,row3_3,row4;
-    LinearLayout Leyout1_2,Leyout1_3;
-    private boolean openMenu=false;
+    LinearLayout Layout3_1, Layout3_2, Layout3_2_1, Layout3_2_2,Layout3_3;
+
+    private boolean openMenu3 =false,openMenu3_2 =false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,17 +30,19 @@ public class Activity_2 extends AppCompatActivity implements View.OnClickListene
         row0=findViewById(R.id.textViewRow0);
         row1=findViewById(R.id.textViewRow1);
         row2=findViewById(R.id.textViewRow2);
-        btn1=findViewById(R.id.imageButton);
 
-        btn1=findViewById(R.id.imageButton);
-        btn1=findViewById(R.id.imageButton);
-        btn1=findViewById(R.id.imageButton);
-        btn1=findViewById(R.id.imageButton);
+        btn3=findViewById(R.id.imageButton3);
+        btn3_2=findViewById(R.id.imageButton3_2);
 
-        Leyout1_2=findViewById(R.id.layout2);
-        Leyout1_3=findViewById(R.id.layout3);
+        Layout3_1 =findViewById(R.id.layout3_1);
+        Layout3_2 =findViewById(R.id.layout3_2);
+        Layout3_2_1 =findViewById(R.id.layout3_2_1);
+        Layout3_2_2 =findViewById(R.id.layout3_2_2);
+        Layout3_3 =findViewById(R.id.layout3_3);
 
-        btn1.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn3_2.setOnClickListener(this);
+
         row0.setOnClickListener(this);
         row1.setOnClickListener(this);
         row2.setOnClickListener(this);
@@ -62,20 +65,38 @@ public class Activity_2 extends AppCompatActivity implements View.OnClickListene
 
         switch (v.getId()) {
 
+            case R.id.textViewRow0:{
+                Intent intent;
 
+                intent = new Intent(Activity_2.this, ScrollingActivity.class );
+                intent.putExtra("textToLoad",R.string.this_is_new);
+                startActivity(intent);
+                // ScrollingActivity.text.setText(R.string.this_is_new);
+                break;
+            }
+            case R.id.textViewRow1:{
+                Intent intent;
+                intent = new Intent(Activity_2.this, ScrollingActivity.class );
+                intent.putExtra("textToLoad",R.string.informationForThosVer);
+                startActivity(intent);               // ScrollingActivity.text.setText(R.string.informationForThosVer);
+                break;
+            }
 //
-            case R.id.imageButton: {
-                if(!openMenu){
-                    btn1.setImageResource(R.drawable.icon_minus);
-                    Leyout1_2.setVisibility(View.VISIBLE);
-                    Leyout1_3.setVisibility(View.VISIBLE);
-                    openMenu=true;
+            case R.id.imageButton3: {
+                if(!openMenu3){
+                    btn3.setImageResource(R.drawable.icon_minus);
+                    Layout3_1.setVisibility(View.VISIBLE);
+                    Layout3_2.setVisibility(View.VISIBLE);
+                    Layout3_3.setVisibility(View.VISIBLE);
+                    openMenu3 =true;
                 }
                 else {
-                    btn1.setImageResource(R.drawable.icon_plus);
-                    Leyout1_2.setVisibility(View.GONE);
-                    Leyout1_3.setVisibility(View.GONE);
-                    openMenu=false;
+                    btn3.setImageResource(R.drawable.icon_plus);
+                    Layout3_1.setVisibility(View.GONE);
+                    Layout3_2.setVisibility(View.GONE);
+                    Layout3_3.setVisibility(View.GONE);
+                    openMenu3 =false;
+                    openMenu3_2 =false;
                     //   btn1.setVisibility(View.INVISIBLE);
                 }
 
@@ -85,22 +106,30 @@ public class Activity_2 extends AppCompatActivity implements View.OnClickListene
 
                 break;
             }
-            case R.id.textViewRow0:{
-                Intent intent;
+            case R.id.imageButton3_2: {
+                if(!openMenu3_2){
+                    btn3_2.setImageResource(R.drawable.icon_minus);
 
-                intent = new Intent(Activity_2.this, ScrollingActivity.class );
-                intent.putExtra("textToLoad",R.string.this_is_new);
-                startActivity(intent);
-               // ScrollingActivity.text.setText(R.string.this_is_new);
+                    Layout3_2_1.setVisibility(View.VISIBLE);
+                    Layout3_2_2.setVisibility(View.VISIBLE);
+                    openMenu3_2 =true;
+                }
+                else {
+                    btn3_2.setImageResource(R.drawable.icon_plus);
+                    Layout3_2_1.setVisibility(View.GONE);
+                    Layout3_2_2.setVisibility(View.GONE);
+
+                    openMenu3_2 =false;
+                    //   btn1.setVisibility(View.INVISIBLE);
+                }
+
+                Toast.makeText(getBaseContext(), "Отваря се  подменю",
+                        Toast.LENGTH_LONG).show();
+
+
                 break;
             }
-            case R.id.textViewRow1:{
-                Intent intent;
-                intent = new Intent(Activity_2.this, ScrollingActivity.class );
-                intent.putExtra("textToLoad",R.string.informationForThosVer);
-                startActivity(intent);               // ScrollingActivity.text.setText(R.string.informationForThosVer);
-                break;
-        }
+
 
         }
     }
