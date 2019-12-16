0dd5e6420ddb6ed9ac6dd826726753a8cef269d7
StanislavGardev <sigma77dev@gmail.com>
2019-12-16 19:04:51 +0200

    Links added

--- app/src/main/java/com/example/healthcare/table_6_6.java
+++ app/src/main/java/com/example/healthcare/table_6_6.java
@@ -1,7 +1,11 @@
 package com.example.healthcare;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,6 +14,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class table_6_6 extends AppCompatActivity {
+    TextView L6_62, L6_63, L6_64, L6_65, L6_66, L6_67;
+    Spanned Link_6_62, Link_6_63, Link_6_64, Link_6_65, Link_6_66, Link_6_67;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,30 @@ public class table_6_6 extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        L6_62 = findViewById(R.id.tableLink_6_62);
+        L6_63 = findViewById(R.id.tableLink_6_63);
+        L6_64 = findViewById(R.id.tableLink_6_64);
+        L6_65 = findViewById(R.id.tableLink_6_65);
+        L6_66 = findViewById(R.id.tableLink_6_66);
+        L6_67 = findViewById(R.id.tableLink_6_67);
+        Link_6_62 = Html.fromHtml(getString(R.string.Link_6_62));
+        Link_6_63 = Html.fromHtml(getString(R.string.Link_6_63));
+        Link_6_64 = Html.fromHtml(getString(R.string.Link_6_64));
+        Link_6_65 = Html.fromHtml(getString(R.string.Link_6_65));
+        Link_6_66 = Html.fromHtml(getString(R.string.Link_6_66));
+        Link_6_67 = Html.fromHtml(getString(R.string.Link_6_67));
+        L6_62.setText(Link_6_62);
+        L6_63.setText(Link_6_63);
+        L6_64.setText(Link_6_64);
+        L6_65.setText(Link_6_65);
+        L6_66.setText(Link_6_66);
+        L6_67.setText(Link_6_67);
+        L6_62.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_63.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_64.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_65.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_66.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_67.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
--- app/src/main/java/com/example/healthcare/table_6_7.java
+++ app/src/main/java/com/example/healthcare/table_6_7.java
@@ -1,7 +1,11 @@
 package com.example.healthcare;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,7 +14,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class table_6_7 extends AppCompatActivity {
-
+    TextView L6_68, L6_70, L6_71;
+    Spanned Link_6_68, Link_6_70, Link_6_71;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,18 @@ public class table_6_7 extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        L6_68 = findViewById(R.id.tableLink_6_68);
+        L6_70 = findViewById(R.id.tableLink_6_70);
+        L6_71 = findViewById(R.id.tableLink_6_71);
+        Link_6_68 = Html.fromHtml(getString(R.string.Link_6_68));
+        Link_6_70 = Html.fromHtml(getString(R.string.Link_6_70));
+        Link_6_71 = Html.fromHtml(getString(R.string.Link_6_71));
+        L6_68.setText(Link_6_68);
+        L6_70.setText(Link_6_70);
+        L6_71.setText(Link_6_71);
+        L6_68.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_70.setMovementMethod(LinkMovementMethod.getInstance());
+        L6_71.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
